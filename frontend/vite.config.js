f64825d073a4c7b5eb6157c65ed6966a60b0e505
Thomas Swokowski <swokowskithomas@gmail.com>
2025-04-10 07:44:34 -0500

    fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,29 +1,31 @@
 import { fileURLToPath, URL } from 'node:url';
 import path from 'path';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-let devToolsPlugin = [];
-if (process.env.NODE_ENV !== 'production') {
-  const vueDevTools = require('vite-plugin-vue-devtools').default;
-  devToolsPlugin = [vueDevTools()];
-}
+export default defineConfig(async () => {
+  const plugins = [vue()];
 
-export default defineConfig({
-  plugins: [vue(), ...devToolsPlugin],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+  if (process.env.NODE_ENV !== 'production') {
+    const { default: vueDevTools } = await import('vite-plugin-vue-devtools');
+    plugins.push(vueDevTools());
+  }
+
+  return {
+    plugins,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  build: {
-    outDir: path.resolve(__dirname, '../backend/public'),
-    emptyOutDir: true,
-  },
-  server: {
-    proxy: {
-      '/api': 'http://localhost:3001',
+    build: {
+      outDir: path.resolve(__dirname, '../backend/public'),
+      emptyOutDir: true,
     },
-  },
+    server: {
+      proxy: {
+        '/api': 'http://localhost:3001',
+      },
+    },
+  };
 });
